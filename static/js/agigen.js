cce0fa0af71716646aeb5f19358888145477d6c2
Daniel Mauno Pettersson <daniel@agigen.se>
2014-08-26 10:29:36 +0200

    static stuff

---
+++ static/js/agigen.js
@@ -0,0 +1,16 @@
+(function($, window, undefined){
+    'use strict';
+
+    angular.module('agigenApp', [])
+    .controller('menuCtrl', ['$scope', function($scope){
+        $scope.menuVisible = false;
+    }]);
+
+
+
+}(jQuery, window));
+
+
+
+
+
