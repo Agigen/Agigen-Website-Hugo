d288d2f23a0080dd2b27653e50c4b5c5bb32e1db
Daniel Mauno Pettersson <daniel@agigen.se>
2014-09-04 17:04:49 +0200

    Map on contact page

--- static/js/agigen.js
+++ static/js/agigen.js
@@ -1,3 +1,5 @@
+var mapsApiKey = "AIzaSyDMMFeNcOLwq4vEFgc9C39sshHtkiVa6jo";
+
 (function($, window, undefined){
     'use strict';
 
@@ -52,7 +54,26 @@
         requestAnimFrame(updateParallax);
     }])
 
+    if (true) {
+        window.initializeGoogleMaps = function() {
+            var mapStyles = [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}];
 
+
+            var mapOptions = {
+                zoom: 15,
+                center: new google.maps.LatLng(59.332779, 18.081026),
+                styles: mapStyles,
+                disableDefaultUI: true,
+                backgroundColor: "#222222"
+            };
+
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        }
+
+        var script = document.createElement('script');
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'callback=initializeGoogleMaps';
+        document.body.appendChild(script);
+    };
 
     $('.animate--in-view').inView({style: 'sticky'});
 }(jQuery, window));
