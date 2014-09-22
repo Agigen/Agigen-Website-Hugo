bf884ce774fbee13640136b960dee5516e4170bf
Daniel Mauno Pettersson <daniel@agigen.se>
2014-09-22 09:56:49 +0200

    blinking cursor

--- site/static/js/agigen.js
+++ site/static/js/agigen.js
@@ -173,6 +173,22 @@ var mapsApiKey = "AIzaSyDMMFeNcOLwq4vEFgc9C39sshHtkiVa6jo";
         chat.onClose = function() { $scope.$apply(function() { $scope.onClosed(); }); };
         chat.onMessage = function(message) { $scope.onIncomingMessage(message); };
 
+        $scope.updateCursorPosition = function(){
+            var n = parseInt($('#screen-input')[0].selectionStart, 10),
+                start = 136,
+                step_width = 7.5;
+            console.log(n);
+            $scope.cursorPositionLeft = start + step_width*n;
+
+            start = 169;
+            step_width = 19;
+            n = $scope.messages.length - 1;
+            console.log($scope.messages, $scope.messages.length)
+            $scope.cursorPositionTop = start + step_width*n;
+        };
+        $scope.keyUp = function(){
+            $scope.updateCursorPosition();
+        };
         $scope.keyDown = function($event){
             var ignoreKeys = [
                 8,
@@ -193,22 +209,26 @@ var mapsApiKey = "AIzaSyDMMFeNcOLwq4vEFgc9C39sshHtkiVa6jo";
                 type = typeAudio.cloneNode();
                 type.play();
             }
+            $scope.updateCursorPosition();
         };
 
         $scope.onConnected = function(){
             $scope.connected = true;
             $scope.messages.push("Connected!");
+            $scope.updateCursorPosition();
         };
 
         $scope.onCloseed = function(){
             $scope.connected = false;
             $scope.messages.push("Connection lost! :(");
+            $scope.updateCursorPosition();
         };
 
         $scope.onIncomingMessage = function(msg) {
             var uhoh;
             $scope.messages.push(msg.username + ": " + msg.text);
             $scope.messages = $scope.messages.slice(-13);
+            $scope.updateCursorPosition();
             $scope.$digest();
             if (msgAudio && msg.username !== $scope.username) {
                 uhoh = msgAudio.cloneNode();
